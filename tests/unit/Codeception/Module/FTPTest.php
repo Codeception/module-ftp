bf82c46b57c663670275064331ca4da88ede27da
Gintautas Miselis <gintautas@miselis.lt>
2022-05-26 08:25:18 +0300

    Support Codeception 5

--- tests/unit/Codeception/Module/FTPTest.php
+++ tests/unit/Codeception/Module/FTPTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Codeception\Module\FTP;
 use Codeception\PHPUnit\TestCase;
-use Codeception\Util\Stub;
+use Codeception\Stub;
 
 final class FTPTest extends TestCase
 {
